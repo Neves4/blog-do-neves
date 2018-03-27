9b581e5b7f8299227b38fe4de78b1acb836e2e1c
neves4 <victorneves478@gmail.com>
2018-03-26 23:22:51 -0300

    Update no script de postagem, para colocar messagem de commit no shell e também melhorias no código.

--- publish-files.sh
+++ publish-files.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
-read -p "Are you sure? Remember to use ./publish-site.sh "\""commit message"\""" -n 1 -r
-echo    # move to a new line
+cr=`echo $'\n.'`
+cr=${cr%.} # Newline para a resposta
 
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
+echo "Only proceed if all files are ready. Continue? (y/n)"
+read -p "" RESP
+
+echo -en "\ec"
+
+if [ "$RESP" = "y" ]; then
+  echo
   echo -e "\033[0;32mPublishing updates to GitHub...\033[0m"
+  echo
 
   git add .   # Stage all files to commit
 
-  msg="Rebuilding site `date`"   # Commit changes to GitHub pages
-  if [ $# -eq 1 ]
-    then msg="$1"
-  fi
-  git commit -m "$msg"
+  echo "Type your commit message:"
+  read -p "" string # Ask the user to enter a string
+
+  git commit -m "$string"
 
   git push origin master   # Push source and build repos.
-
-  cd ..   # Come Back up to the Project Root
+else
+  echo "Finish your modifications before using this script."
 fi
