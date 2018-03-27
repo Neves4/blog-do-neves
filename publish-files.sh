e901f72ecec2a1f0045c44895c3ded2fe365af73
neves4 <victorneves478@gmail.com>
2018-03-26 22:55:26 -0300

    Rebuilding site Mon, Mar 26, 2018 10:55:26 PM

--- publish-files.sh
+++ publish-files.sh
@@ -1,26 +1,21 @@
 #!/bin/bash
 
-ls
+read -p "Are you sure? Remember to use ./publish-site.sh "\""commit message"\""" -n 1 -r
+echo    # move to a new line
 
-read -p "Continue (y/n)?" choice
-case "$choice" in
-  y|Y ) echo "yes"
-    echo -e "\033[0;32mPublishing updates to GitHub...\033[0m"
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  echo -e "\033[0;32mPublishing updates to GitHub...\033[0m"
 
-    git add .   # Stage all files to commit
+  git add .   # Stage all files to commit
 
-    msg="Rebuilding site `date`"   # Commit changes to GitHub pages
-    if [ $# -eq 1 ]
-      then msg="$1"
-    fi
-    git commit -m "$msg"
+  msg="Rebuilding site `date`"   # Commit changes to GitHub pages
+  if [ $# -eq 1 ]
+    then msg="$1"
+  fi
+  git commit -m "$msg"
 
-    git push origin master   # Push source and build repos.
-    ;;
-  n|N ) echo "no"
-    echo -e "Review your input."
-    ;;
-  * ) echo "invalid"
-    echo -e "WTF??!"
-    ;;
-esac
+  git push origin master   # Push source and build repos.
+
+  cd ..   # Come Back up to the Project Root
+fi
